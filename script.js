dc43ca33d1fbb5c601d4edf69951fb85fcaa4030
Kenneth Cheng <itskcheng@gmail.com>
2021-04-23 23:47:53 -0400

    Set up the arrays needed before creating the logic

--- script.js
+++ script.js
@@ -12,3 +12,14 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//Arrays of uppercase alphabets, lowercase alphabets, numbers and special characters
+var uppercaseAlphabets = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var lowercaseAlphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var specialCharacters = ['!', '”', '#', '$', '%', '&', '’', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '^', '_', '|'];
+//Empty arrays that the password, reserved words and all the four arrays will be added in one array.
+var newPassword = [];
+var reservedWords = [];
+var container = [];
+
