c5372c883f49bd42075dcd98ee2100522bb6eee9
Kenneth Cheng <itskcheng@gmail.com>
2021-04-24 00:00:35 -0400

    Create the inital prompt and if statments to check if the user has selected a number within the criteria

--- script.js
+++ script.js
@@ -23,3 +23,32 @@ var newPassword = [];
 var reservedWords = [];
 var container = [];
 
+//Creating the random number generator function that grabs a letter depending what is added to the parameter
+
+function randomNumber(array) {
+  var index = Math.floor(Math.random() * array.length);
+  var store = array[index];
+  return store;
+}
+console.log(randomNumber(uppercaseAlphabets));
+
+function generatePassword() {
+
+  //First prompt that ask you to enter the amount of characters you want for your password
+  var passwordLength = prompt('How many characters would you like your password to contain?');
+
+  //If statment that check if the password is greater then or equal to 8 and less then or equal to 128. If it matches the criteria then continue if not then send an alert and it runs the function again. 
+
+  if (passwordLength >= 8 && passwordLength <= 128) {
+
+    //Confrim questions that will come out as true or false
+    var uppercaseQuestion = window.confirm('Do you want uppercase characters?');
+    var lowercaseQuestion = window.confirm('Do you want lowercase characters?');
+    var numberQuestion = window.confirm('Do you want numerical values?');
+    var specialCharacterQuestion = window.confirm('Do you want special characters?');
+
+  } else {
+    alert('Your password is either less then 8 character or greater then 128 characters please try again.');
+    generatePassword();
+  }
+}
